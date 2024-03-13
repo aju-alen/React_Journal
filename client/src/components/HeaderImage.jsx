e58dc0f9bb41c113936de55c503b0c012530a8cf
Alen <alenaju1304@gmail.com>
2024-03-13 11:55:07 +0400

    typo fixes

--- client/src/components/HeaderImage.jsx
+++ client/src/components/HeaderImage.jsx
@@ -17,7 +17,7 @@ const HeaderImage = ()=>{
       />
       
       <div className="absolute sm:top-40 md:top-1/4 top-1/4  left-1/2  transform -translate-x-1/2 -translate-y-1/2 text-gray-900 text-center w-full flex flex-col justify-center items-center gap-3 ">
-        <h1 className=" text-3xl font-bold font sm:text-4xl md:text-6xl">Scientific Jounal</h1>
+        <h1 className=" text-3xl font-bold font sm:text-4xl md:text-6xl">Scientific Journal</h1>
         {/* <p className="text-lg text-center w-1/2">Paragraph about the scientific Journal  </p> */}
         {/* <Link to="/journal" className=" text-lg font-bold border-2 rounded p-4 border-black bg-black md:w-1/6 text-white"> */}
         {/* <button onClick={handleClickJournal}>
