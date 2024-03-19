dcfe89d169c824e44396a9f047a0085f73d6b312
Alen <alenaju1304@gmail.com>
2024-03-19 11:02:07 +0400

    added default url

--- api/utils/cors.dev.js
+++ api/utils/cors.dev.js
@@ -1,1 +1,3 @@
 export const originUrl = ['https://scientificjournalsportal.com', 'https://react-journal-1.onrender.com'];
+
+export const emailVerifyBackendUrl = 'https://react-journal.onrender.com';
