b82dc51020ed88c725984bfb6fa67cc098210ccd
Alen <alenaju1304@gmail.com>
2024-04-04 08:33:03 +0400

    add url

--- api/utils/cors.dev.js
+++ api/utils/cors.dev.js
@@ -1,3 +1,3 @@
-export const originUrl = 'https://scientificjournalsportal.com';
+export const originUrl = ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com'];
 
-export const emailVerifyBackendUrl = 'https://react-journal.onrender.com'
+export const emailVerifyBackendUrl = 'https://react-journal1.onrender.com'
