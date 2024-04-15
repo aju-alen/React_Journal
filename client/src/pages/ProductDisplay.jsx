31c7d45042a0d775ba6e66e4d282beeeb002c8e3
Alen <alenaju1304@gmail.com>
2024-04-15 11:11:45 +0400

    Subscription pricing table added

--- client/src/pages/ProductDisplay.jsx
+++ client/src/pages/ProductDisplay.jsx
@@ -1,8 +1,81 @@
 
 import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import Chip from '@mui/material/Chip';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
 import ImageHeader from '../components/ImageHeader';  
 import { httpRoute } from '../helperFunctions.js';
-const ProductDisplayy = () => (
+
+
+const tiers = [
+  {
+    title: 'Free',
+    price: '0',
+    description: [
+      '10 users included',
+      '2 GB of storage',
+      'Help center access',
+      'Email support',
+    ],
+    buttonText: 'Sign up for free',
+    buttonVariant: 'outlined',
+  },
+  {
+    title: 'Starter plan',
+    subheader: 'Recommended',
+    price: '20.00',
+    description: [
+      '20 users included',
+      '10 GB of storage',
+      'Help center access',
+      'Priority email support',
+      'Dedicated team',
+      'Best deals',
+    ],
+    buttonText: 'Start now',
+    buttonVariant: 'contained',
+  },
+  {
+    title: 'Enterprise',
+    price: '30',
+    description: [
+      '50 users included',
+      '30 GB of storage',
+      'Help center access',
+      'Phone & email support',
+    ],
+    buttonText: 'Contact us',
+    buttonVariant: 'outlined',
+  },
+];
+
+
+const ProductDisplayy = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("https://react-journal1.onrender.com/api/stripe/create-checkout-sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ lookup_key: "ArticlePurchase" })
+      });
+      // Handle response as needed
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  return (
     <div className="h-auto w-auto bg-slate-200 ">
     <ImageHeader/>
     <section>
@@ -21,10 +94,167 @@ const ProductDisplayy = () => (
       </button>
     </form>
   </section>
+
+  <Container
+      id="pricing"
+      sx={{
+        pt: { xs: 4, sm: 12 },
+        pb: { xs: 8, sm: 16 },
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: { xs: 3, sm: 6 },
+      }}
+    >
+      <Box
+        sx={{
+          width: { sm: '100%', md: '60%' },
+          textAlign: { sm: 'left', md: 'center' },
+        }}
+      >
+        <Typography component="h2" variant="h4" color="text.primary">
+          Pricing
+        </Typography>
+        <Typography variant="body1" color="text.secondary">
+          Quickly build an effective pricing table for your potential customers with
+          this layout. <br />
+          It&apos;s built with default Material UI components with little
+          customization.
+        </Typography>
+      </Box>
+      <Grid container spacing={3} alignItems="center" justifyContent="center">
+        {tiers.map((tier) => (
+          <Grid
+            item
+            key={tier.title}
+            xs={12}
+            sm={tier.title === 'Enterprise' ? 12 : 6}
+            md={4}
+          >
+            <Card
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 4,
+                border: tier.title === 'Professional' ? '1px solid' : undefined,
+                borderColor:
+                  tier.title === 'Professional' ? 'primary.main' : undefined,
+                background:
+                  tier.title === 'Professional'
+                    ? 'linear-gradient(#033363, #021F3B)'
+                    : undefined,
+              }}
+            >
+              <CardContent>
+                <Box
+                  sx={{
+                    mb: 1,
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    color: tier.title === 'Professional' ? 'grey.100' : '',
+                  }}
+                >
+                  <Typography component="h3" variant="h6">
+                    {tier.title}
+                  </Typography>
+                  {tier.title === 'Professional' && (
+                    <Chip
+                      icon={<AutoAwesomeIcon />}
+                      label={tier.subheader}
+                      size="small"
+                      sx={{
+                        background: (theme) =>
+                          theme.palette.mode === 'light' ? '' : 'none',
+                        backgroundColor: 'primary.contrastText',
+                        '& .MuiChip-label': {
+                          color: 'primary.dark',
+                        },
+                        '& .MuiChip-icon': {
+                          color: 'primary.dark',
+                        },
+                      }}
+                    />
+                  )}
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'baseline',
+                    color: tier.title === 'Professional' ? 'grey.50' : undefined,
+                  }}
+                >
+                  <Typography component="h3" variant="h2">
+                    ${tier.price}
+                  </Typography>
+                  <Typography component="h3" variant="h6">
+                    &nbsp; per month
+                  </Typography>
+                </Box>
+                <Divider
+                  sx={{
+                    my: 2,
+                    opacity: 0.2,
+                    borderColor: 'grey.500',
+                  }}
+                />
+                {tier.description.map((line) => (
+                  <Box
+                    key={line}
+                    sx={{
+                      py: 1,
+                      display: 'flex',
+                      gap: 1.5,
+                      alignItems: 'center',
+                    }}
+                  >
+                    <CheckCircleRoundedIcon
+                      sx={{
+                        width: 20,
+                        color:
+                          tier.title === 'Professional'
+                            ? 'primary.light'
+                            : 'primary.main',
+                      }}
+                    />
+                    <Typography
+                      component="text"
+                      variant="subtitle2"
+                      sx={{
+                        color:
+                          tier.title === 'Professional' ? 'grey.200' : undefined,
+                      }}
+                    >
+                      {line}
+                    </Typography>
+                  </Box>
+                ))}
+              </CardContent>
+              <CardActions>
+      <form onSubmit={handleSubmit}>
+        {/* Add a hidden field with the lookup_key of your Price */}
+        <input type="hidden" name="lookup_key" value="ArticlePurchase" />
+        <Button
+          fullWidth
+          variant={tier.buttonVariant}
+          type="submit"
+        >
+          {tier.buttonText}
+        </Button>
+      </form>
+    </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
     </div>
 
     
 )
+}
 
 const SuccessDisplay = ({ sessionId }) => {
 
