8b1154b9da592d64b6d4b76d16a4ecec6a22c460
Alen <alenaju1304@gmail.com>
2024-05-26 11:57:49 +0400

    changed about us content

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,7 +13,20 @@ const Home = () => {
       <ServicesHome />
       <div className='mt-4'>
         <h2 className=" text-center font-bold text-3xl">About Us</h2>
-        <p className=' text-justify p-10 md:px-44'>Scientific Journals Portal serves as a publisher for a wide range of open access journals that undergo rigorous peer review. With a diverse collection of over 100 journals, we cover various disciplines including art and humanities, engineering, medical science, social sciences, biological sciences, physical sciences, and agricultural sciences. Our primary objective is to expedite the sharing of knowledge by publishing research articles of exceptional quality, while adhering to the principles of open access.</p>
+        <p className=' text-justify p-10 md:px-44'>Scientific Journals Portal (SJP) serves as a publisher for a wide
+range of hybrid – open and subscription access journals that
+undergo rigorous peer review. With a diverse collection of journals,
+and journal publications, Scientific Journals Portal (SJP), covers
+various disciplines including arts and humanities, engineering,
+medical science, social sciences, biological sciences, physical
+sciences, and agricultural sciences. The portal&#39;s purpose is to
+provide a geo-agnostic springboard for research across multiple
+disciplines through peer reviewed publications supported by our
+able editorial board which consists of personas of reputable
+academia pedigree. The primary objective of SJP is to expedite the
+sharing of knowledge by publishing research articles of
+exceptional quality, while adhering to the principles of knowledge
+sharing and accessibility for the benefit of all.</p>
       </div>
 <div className="mt-4 ">
       <EthicsOfPublicationCards />
