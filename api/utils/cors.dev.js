5a65f19090a6dcc5c8cdfcbb320f6b78860ea70b
aju-alen <alenaju1304@gmail.com>
2024-11-27 15:52:38 +0400

    added cors url

--- api/utils/cors.dev.js
+++ api/utils/cors.dev.js
@@ -1,4 +1,4 @@
-export const originUrl = ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com','http://localhost:5173']; // ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com']
+export const originUrl = ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com','https://rightintellectual.com']; // ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com']
 export const emailVerifyBackendUrl = "https://react-journal1.onrender.com" //'https://react-journal1.onrender.com'
 
 export const stripeDomain = "https://scientificjournalsportal.com"
