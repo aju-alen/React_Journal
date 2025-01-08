c89e9faa7359c12de7233bebf92c101a12ed6707
aju-alen <alenaju1304@gmail.com>
2025-01-08 18:55:10 +0400

    export bug fix

--- api/utils/cors.dev.js
+++ api/utils/cors.dev.js
@@ -1,8 +1,7 @@
-export const originUrl = ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com','https://rightintellectual.com']; // ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com']
+export const originUrl = ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com','https://rightintellectual.com','http://localhost:5173']; // ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com']
 export const emailVerifyBackendUrl = "https://react-journal1.onrender.com" //'https://react-journal1.onrender.com'
-
-const emailUrl = 'https://scientificjournalsportal.com'
-// const emailUrl = 'http://localhost:5173'
+export const emailUrl = 'https://scientificjournalsportal.com'
+// export const emailUrl = 'http://localhost:5173'
 
 export const stripeDomain = "https://scientificjournalsportal.com"
 // export const stripeDomain = "http://localhost:5173"
