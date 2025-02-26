183b966d2431a5a7711cadf219d49904320e6258
aju-alen <alenaju1304@gmail.com>
2025-02-26 10:20:43 +0400

    refactor(dev-prod-env): dynamic dev-prod url

--- api/utils/cors.dev.js
+++ api/utils/cors.dev.js
@@ -1,7 +1,7 @@
 export const originUrl = ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com','https://rightintellectual.com','http://localhost:5173']; // ['https://scientificjournalsportal.com','https://react-journal-i1ca.onrender.com']
-export const emailVerifyBackendUrl = "https://react-journal1.onrender.com" //'https://react-journal1.onrender.com'
-export const emailUrl = 'https://scientificjournalsportal.com'
-// export const emailUrl = 'http://localhost:5173'
 
-export const stripeDomain = "https://scientificjournalsportal.com"
-// export const stripeDomain = "http://localhost:5173"
+export const emailVerifyBackendUrl = process.env.REACT_APP_EMAIL_VERIFY_BACKEND_URL || "http://localhost:3001"
+
+export const emailUrl = process.env.REACT_APP_EMAIL_URL || 'http://localhost:5173'
+
+export const stripeDomain = process.env.REACT_APP_STRIPE_DOMAIN || "http://localhost:5173"
